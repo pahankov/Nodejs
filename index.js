86db8ee6735558f7fffb5c0193b10b60b07ef9e8
Pahankov <Pahankov@mail.ru>
2026-04-05 14:27:20 +0300

    wrong eslint

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const sum = (a, b) => a + b;
 let x = 10;
 let y = 20;
 
-console.log(sum(x, y)); // 30
+console.log(sum(x, y)) // 30
     debugger;
 x = 20;
 y = 30;
 
-console.log(sum(x, y)); // 50
+console.log(sum(x,      y)); // 50
 };
 
 func();
